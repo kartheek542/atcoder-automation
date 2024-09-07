31b2e4c493762cb250b191ef3253b49151dcb580
kartheek542 <kartheekdivvela@gmail.com>
2024-09-07 09:34:16 +0530

    Need to iterate the problems and get the input I/O

--- prepare-contest.sh
+++ prepare-contest.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "this is preare-contest.sh"
+contest="arc183"
+curl -s https://atcoder.jp/contests/$contest/tasks | grep "/contests/$contest/tasks/$contest" | sed -Ee 's/^.*<a.*"(.+)".*$/\1/g' | uniq > $contest.problems
+
+for line in $(cat $contest.problems);
+do
+    problem_url="https://atcoder.jp$line"
+    echo $problem_url
+done
+
+rm -f $contest.problems
