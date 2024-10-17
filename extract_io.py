76628fc16dd8ad46ae86449927061c10d3c5e358
kartheek542 <64679923+kartheek542@users.noreply.github.com>
2024-10-17 16:50:46 +0530

    Update extract_io.py so that exact input and ouput only are extracted

--- extract_io.py
+++ extract_io.py
@@ -8,13 +8,17 @@ n = len(file)
 io = []
 curr = ""
 load = False
+flag = False
 
 for i in range(5, n):
-    if file[i - 5: i] == "<pre>":
+    if file[i - 6: i] == "Sample":
+        flag = True
+    if flag and file[i - 5: i] == "<pre>":
         load = True
     elif file[i: i + 6] == "</pre>":
         io.append(curr)
         load = False
+        flag = False
         curr = ""
     if load:
         curr = f"{curr}{file[i]}"
